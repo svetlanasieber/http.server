369baa92b6c3075fe9d4cee9a3e135d4e929532a
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-01 23:37:39 +0200

    Update http_server.py

--- http_server.py
+++ http_server.py
@@ -5,7 +5,7 @@ class SimpleHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
-        self.wfile.write(b"Hello from pure Python web server!")
+        self.wfile.write(b"Test from pure Python web server!")
 
 httpd = HTTPServer(('localhost', 8000), SimpleHandler)
 print("Running on http://localhost:8000/")
