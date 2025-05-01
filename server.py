934e56da6a41f37ebf011edec50d23ec704bddd8
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-01 22:24:50 +0200

    Update server.py

--- server.py
+++ server.py
@@ -8,6 +8,11 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=DIRECTORY, **kwargs)
 
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+        return super().do_GET()
+
 with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
-    print(f"Serving files at http://localhost:{PORT}")
+    print(f"Serving at http://localhost:{PORT}")
     httpd.serve_forever()
